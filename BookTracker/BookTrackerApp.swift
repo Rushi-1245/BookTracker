38b6d71b6170644ad9213d9c9fe08712e4bec3ac
Rushi-1245 <rushikeshgaikwad4899@gmail.com>
2025-07-22 10:36:27 +0530

    Initial commit: setup SwiftUI + Core Data project

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by Siddhatech on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
